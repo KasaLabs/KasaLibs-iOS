78c602ecffd153fb4e7e6bcc16538a195da1c4ae
Suleyman Calik <suleymancalik@gmail.com>
2015-08-04 23:01:14 +0300

    Deneme.swift

---
+++ Classes/Deneme.swift
@@ -0,0 +1,13 @@
+//
+//  Deneme.swift
+//  KasaTest
+//
+//  Created by Suleyman Calik on 04/08/15.
+//  Copyright (c) 2015 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class Deneme: NSObject {
+   
+}
